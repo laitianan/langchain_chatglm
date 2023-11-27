06c64f983a3b4c0ce350a0d559778e5c6000b35e
laitianan <986088865@qq.com>
2023-11-27 09:58:49 +0800

    prompt_helper.py 修改多意图模板

--- prompt_helper.py
+++ prompt_helper.py
@@ -1,10 +1,10 @@
-
 from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 from langchain.prompts import PromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
-from api_protocol import  (
-InitInterfaceRequest,
-Interface
+from api_protocol import (
+    InitInterfaceRequest,
+    Interface
 )
+
 FUNTION_FORMAT_INSTRUCTIONS = """
 你的任务是根据***分隔符的历史对话沟通记录，从user和assistant聊天记录理解user需求，并从聊天记录抽取正确的结构值以结构化数据格式返回，取不到的值使用None代替,
 部分时间直接从user聊天抽取不出来才结合系统当前时间推理,系统当前时间{current_time},比如则根据系统当前时间推理:今天的开始日期时间为{current_date} 00:00:00,今天的结束日期时间为{current_date} 23:59:59,严格禁止生成聊天记录不存在的数据,
@@ -16,30 +16,34 @@ FUNTION_FORMAT_INSTRUCTIONS = """
 ***
 你的回答:
 """
-def create_fun_prompt(param:Interface)->PromptTemplate:
-    response_schemas = [ResponseSchema(name=p.name,type=p.type, description=f'{p.title}，抽取不到时使用"None"代替') for p in param.inputParams]
+
+
+def create_fun_prompt(param: Interface) -> PromptTemplate:
+    response_schemas = [ResponseSchema(name=p.name, type=p.type, description=f'{p.title}，抽取不到时使用"None"代替') for
+                        p in param.inputParams]
     output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
     # 获取响应格式化的指令
     format_instructions = output_parser.get_format_instructions(only_json=True)
     prompt = PromptTemplate(
-        input_variables=["user_input","current_time","current_date"],
+        input_variables=["user_input", "current_time", "current_date"],
         partial_variables={"format_instructions": format_instructions},
         template=FUNTION_FORMAT_INSTRUCTIONS
     )
-    return param.id,prompt
+    return param.id, prompt
 
-def init_all_fun_prompt(parmas:InitInterfaceRequest):
-    prompt_dict=dict()
+
+def init_all_fun_prompt(parmas: InitInterfaceRequest):
+    prompt_dict = dict()
     for param in parmas.params:
         if param.usableFlag:
-            id,prompt=create_fun_prompt(param)
-            prompt_dict[id]=prompt
+            id, prompt = create_fun_prompt(param)
+            prompt_dict[id] = prompt
     return prompt_dict
 
 
 INTENT_FORMAT_MULTI_INSTRUCTIONS: str = """
 现在有一些意图，类别为：{intents}，
-你扮演AI角色的任务是根据***分隔符的文本对话，来进行意图的识别`，对话中可能存在一个或多个意图，使用列表方式返回,比如 intention_name:["a","b"]。
+你扮演AI角色的任务是根据***分隔符的文本对话，通过理解或者user聊天的关键词模糊匹配，比如聊天记录含有业绩就匹配类别中所有含业绩该关键词所有意图，使用列表方式返回,比如 intention_name:["a","b"]。
 输出应该是严格按以下模式格式化的标记代码片段，必须包括开头和结尾的" ' ' ' json"和" ' ' ' ":
 ```json
 {{
@@ -54,7 +58,6 @@ INTENT_FORMAT_MULTI_INSTRUCTIONS: str = """
 ***
 你的回答:
 """
-
 
 INTENT_FORMAT_INSTRUCTIONS: str = """
 现在有一些意图，类别为：{intents}，
@@ -74,11 +77,7 @@ INTENT_FORMAT_INSTRUCTIONS: str = """
 你的回答:
 """
 
-
-
-
 if __name__ == '__main__':
-
     from MyOpenAI import myOpenAi
 
-    llm = myOpenAi(temperature=0.8,max_tokens=2000)
+    llm = myOpenAi(temperature=0.8, max_tokens=2000)
